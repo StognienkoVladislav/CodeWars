9393d0499c10ac0b05be35804f9d4c281e88fac1
StognienkoVladislav <stognienkovv@gmail.com>
2017-07-30 19:04:55 +0400

    not fun(

--- BasicDeNico.py
+++ BasicDeNico.py
@@ -1,35 +1,62 @@
 
 def de_nico(key, msg):
     n = [[] for i in range(len(key))]
+    pkey = key
+    #print(pkey)
+    key = sorted(key)
+
+    #print(key)
+    #print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1')
 
     l=0
     for x in key:
         n[l].append(x)
         l += 1
+    print(n)
 
-    y = msg.replace(" ", "")
-    y = y.split()
+    #y = msg.replace(" ", "")
+    #print(y)
 
-    for z in y:
-        for j in range(len(key)):
-            n[j].append(z)
+    t=0
+    for z in msg:
+        if t > (len(key) - 1):          # запись в лист
+            t = 0
+        n[t].append(z)
+        t+=1
 
-    n.sort()
+    print(n)
 
     msg = ""
 
-    for x in range(len(key)):
-        for k in range(len(n[x])):
-            msg += n[x[k]]
+    #for x in n:
+    #    for k in range(len(n[x])):  #Лист листов
+    #        msg += x[k+1]
 
-    return msg
+    z = 1
+
+    r = []
+
+    for j in pkey:                      #Swap Относительно ключа
+        for y in n:
+            if j == y[0]:
+                r.append(y)
+
+    print(r)
+
+
+
+    for j in range(r[0].__len__() - 1):
+        for x in r:
+            msg += x[j+1]
+
+    return msg.replace(" ", "")
 
 
 
 if __name__ == '__main__':
-    print(de_nico("crazy", "cseerntiofarmit on  "), "secretinformation")
-    print(de_nico("crazy", "cseerntiofarmit on"), "secretinformation")
-    print(de_nico("abc", "abcd"), "abcd")
-    print(de_nico("ba", "2143658709"), "1234567890")
-    print(de_nico("a", "message"), "message")
-    print(de_nico("key", "eky"), "key")
+    print(de_nico("crazy", "cseerntiofarmit on  "))
+    #print(de_nico("crazy", "cseerntiofarmit on"), "secretinformation")
+    #print(de_nico("abc", "abcd"), "abcd")
+    #print(de_nico("ba", "2143658709"), "1234567890")
+    #print(de_nico("a", "message"), "message")
+    #print(de_nico("key", "eky"), "key")
